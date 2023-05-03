9e3761eb08b0d55f982b0f33ce6b7bd5ac87706f
mdykiel <miro24@gmail.com>
2023-05-03 20:40:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
+//export default defineConfig({
+//    plugins: [vue()],
+//});
+// https://vitejs.dev/config/
 export default defineConfig({
+    resolve: { alias: { '@': '/src' } },
     plugins: [vue()],
-});
+    server: {
+        host: true,
+        port: 8080
+    }
+})
