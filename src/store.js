3216a9aa69265b22f4be753d05142b4da2fefc00
mdykiel <miro24@gmail.com>
2023-05-04 22:30:48 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -28,7 +28,7 @@ export const store = reactive({
   getData() {
     this.loading = true;
     fetch(
-      `http://192.168.160.2:8080/questions.json`
+      `http://127.0.0.1:8080/questions.json`
     )
       .then((res) => res.json())
       .then((res) => {
