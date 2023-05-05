032db43470966cd6e4c3965f6e96bbf8c855a16c
mdykiel <miro24@gmail.com>
2023-05-05 21:19:38 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -28,7 +28,7 @@ export const store = reactive({
   getData() {
     this.loading = true;
     fetch(
-      `http://146.59.86.242:8080/questions.json`
+      `https://bizneto.pl/data.json`
     )
       .then((res) => res.json())
       .then((res) => {
